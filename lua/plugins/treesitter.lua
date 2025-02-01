08186842896f2d6406761bd60b74cbcaf0425433
nhaef <me@nico.icu>
2025-02-01 13:09:55 +0100

    minor refactoring

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,16 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		init = function()
-			require("nvim-treesitter.configs").setup {
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "kdl", "jsonc", "typst" },
-				sync_install = false,
-				auto_install = false,
-				highlight = {
-					enable = true,
-				},
-			}
-		end
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        init = function()
+            require("nvim-treesitter.configs").setup {
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "kdl", "jsonc", "typst" },
+                sync_install = false,
+                auto_install = false,
+                highlight = {
+                    enable = true,
+                },
+            }
+        end
+    },
 }
