bfe09ad8722b7dbecf47f3d21d44483c6476c228
Nico Haefele <git@nhaef.de>
2026-02-25 15:32:16 +0100

    refactor: make colorscheme and statusline responsive to light/dark mode

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -12,9 +12,27 @@ return {
                         mantle = "#101010",
                         crust = "#202020",
                     },
+                    latte = {
+                        base = "#FFFFFF",
+                        mantle = "#F0F0F0",
+                        crust = "#E0E0E0",
+                    },
                 }
-            });
-            vim.cmd.colorscheme("catppuccin-mocha")
+            })
+            vim.cmd.colorscheme("catppuccin")
         end,
-    }
+    },
+    {
+        "f-person/auto-dark-mode.nvim",
+        opts = {
+            update_interval = 1000,
+            fallback = "dark",
+            set_dark_mode = function()
+                vim.api.nvim_set_option_value("background", "dark", {})
+            end,
+            set_light_mode = function()
+                vim.api.nvim_set_option_value("background", "light", {})
+            end,
+        },
+    },
 }
--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -3,16 +3,10 @@ return {
     name = "lualine",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     init = function()
-        local custom_16color = require("lualine.themes.16color")
-        local colors = {
-            dark_gray = "#282828",
-        }
-        custom_16color.normal.b.bg = colors.dark_gray
-        custom_16color.inactive.b.bg = colors.dark_gray
         require("lualine").setup {
             options = {
                 icons_enabled = true,
-                theme = custom_16color,
+                theme = "catppuccin",
                 component_separators = {
                     left = "|",
                     right = "|",
